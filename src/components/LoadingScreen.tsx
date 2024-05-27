e80c22aba7b883d11acf90c0fdceb5fb6226d348
André Sousa <andrels9283@gmail.com>
2024-05-27 13:43:29 +0100

    adjust spinner color again

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import Spinner from "./Spinner";
 const LoadingScreen = () => {
   return (
     <div className="dark:bg-dark bg-light fixed top-0 left-0 right-0 bottom-0 w-full h-full flex justify-center items-center z-[9999]">
-      <Spinner size="h-28 w-28" color="primary" />
+      <Spinner size="h-28 w-28" color="border-primary" />
     </div>
   );
 };
